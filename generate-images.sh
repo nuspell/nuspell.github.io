08f3f61d5a3ef69c7ef3e868e81e3530178c2267
PanderMusubi <pander@users.sourceforge.net>
2020-01-16 14:07:48 +0100

    housekeeping

--- generate-images.sh
+++ generate-images.sh
@@ -5,11 +5,18 @@
 cd assets/images
 for i in *svg; do
 	DST=`basename $i .svg`'-512x512.png'
-	inkscape -f $i -e $DST
+	inkscape -z -f $i -e $DST 2>&1 >/dev/null
 	optipng -quiet $DST -out $DST
 done
-inkscape -w 400 -h 400 -f logo-trans.svg -e favicon-400x400.png
+
+rm -f sticker-hexagon-512x512.png # TODO git rm
+inkscape -z -w 443 -h 512 -f sticker-hexagon.svg -e sticker-hexagon.eps 2>&1 >/dev/null
+inkscape -z -w 443 -h 512 -f sticker-hexagon.svg -e sticker-hexagon-443x512.png 2>&1 >/dev/null
+optipng -quiet sticker-hexagon-443x512.png -out sticker-hexagon-443x512.png
+
+inkscape -z -w 400 -h 400 -f logo-trans.svg -e favicon-400x400.png 2>&1 >/dev/null
 optipng -quiet favicon-400x400.png -out favicon-400x400.png
-inkscape -w 48 -h 48 -f logo-trans.svg -e favicon-48x48.png
+
+inkscape -z -w 48 -h 48 -f logo-trans.svg -e favicon-48x48.png 2>&1 >/dev/null
 optipng -quiet favicon-48x48.png -out favicon-48x48.png
 convert favicon-48x48.png ../../favicon.ico
