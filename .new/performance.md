f0a007e93bef131ebf135d0b001c0f4124174cd9
Dimitrij Mijoski <dmjpp@hotmail.com>
2020-11-13 22:25:32 +0100

    Delete obsolete stuff

--- .new/performance.md
+++
@@ -1,8 +0,0 @@
----
-title: Performance
-layout: page
----
-
-# See also
-
-Upcoming features improving performance are listed in the project's [roadmap](roadmap.html).
--- .new/update-uml-doc.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-rm -rf uml
-mkdir uml
-cp ../misc-nuspell/uml/usecase-diagrams/??-*-*-usecase-diagram.svg uml
-cp ../misc-nuspell/uml/activity-diagrams/??-*-*-activity-diagram.svg uml
-cp ../misc-nuspell/uml/class-diagrams/??-*-*-class-diagram.svg uml
